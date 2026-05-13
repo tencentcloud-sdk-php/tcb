398d37dd0a1d59b95f722af6fd50910f01c3b3d3
tencentcloudapi <tencentcloudapi@tencent.com>
2026-05-14 04:36:29 +0800

    release 3.0.1587

---
+++ src/TencentCloud/Tcb/V20180608/Models/AllocateEnvResponse.php
@@ -0,0 +1,77 @@
+<?php
+/*
+ * Copyright (c) 2017-2025 Tencent. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+namespace TencentCloud\Tcb\V20180608\Models;
+use TencentCloud\Common\AbstractModel;
+
+/**
+ * AllocateEnv返回参数结构体
+ *
+ * @method string getEnvId() 获取<p>环境ID</p>
+ * @method void setEnvId(string $EnvId) 设置<p>环境ID</p>
+ * @method string getExternalAppId() 获取<p>回显    客户平台的应用标识，如果没有则不传</p>
+ * @method void setExternalAppId(string $ExternalAppId) 设置<p>回显    客户平台的应用标识，如果没有则不传</p>
+ * @method string getRequestId() 获取唯一请求 ID，由服务端生成，每次请求都会返回（若请求因其他原因未能抵达服务端，则该次请求不会获得 RequestId）。定位问题时需要提供该次请求的 RequestId。
+ * @method void setRequestId(string $RequestId) 设置唯一请求 ID，由服务端生成，每次请求都会返回（若请求因其他原因未能抵达服务端，则该次请求不会获得 RequestId）。定位问题时需要提供该次请求的 RequestId。
+ */
+class AllocateEnvResponse extends AbstractModel
+{
+    /**
+     * @var string <p>环境ID</p>
+     */
+    public $EnvId;
+
+    /**
+     * @var string <p>回显    客户平台的应用标识，如果没有则不传</p>
+     */
+    public $ExternalAppId;
+
+    /**
+     * @var string 唯一请求 ID，由服务端生成，每次请求都会返回（若请求因其他原因未能抵达服务端，则该次请求不会获得 RequestId）。定位问题时需要提供该次请求的 RequestId。
+     */
+    public $RequestId;
+
+    /**
+     * @param string $EnvId <p>环境ID</p>
+     * @param string $ExternalAppId <p>回显    客户平台的应用标识，如果没有则不传</p>
+     * @param string $RequestId 唯一请求 ID，由服务端生成，每次请求都会返回（若请求因其他原因未能抵达服务端，则该次请求不会获得 RequestId）。定位问题时需要提供该次请求的 RequestId。
+     */
+    function __construct()
+    {
+
+    }
+
+    /**
+     * For internal only. DO NOT USE IT.
+     */
+    public function deserialize($param)
+    {
+        if ($param === null) {
+            return;
+        }
+        if (array_key_exists("EnvId",$param) and $param["EnvId"] !== null) {
+            $this->EnvId = $param["EnvId"];
+        }
+
+        if (array_key_exists("ExternalAppId",$param) and $param["ExternalAppId"] !== null) {
+            $this->ExternalAppId = $param["ExternalAppId"];
+        }
+
+        if (array_key_exists("RequestId",$param) and $param["RequestId"] !== null) {
+            $this->RequestId = $param["RequestId"];
+        }
+    }
+}
